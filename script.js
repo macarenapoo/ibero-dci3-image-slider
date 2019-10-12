22037446d364fce8479e8e686ecfcfade2faf63a
Nicole K <nicolekleimanyedid@gmail.com>
2019-10-12 10:08:48 -0500

    Intento que no me sale todavía

---
+++ script.js
@@ -0,0 +1,17 @@
+function mostrarImagenUno() {
+  var wrapper = document.getElementsByClassName("slider-slides-wrapper");
+wrapper.style.marginLeft="0px";
+
+}
+
+function mostrarImagenDos() {
+  var wrapper = document.getElementsByClassName("slider-slides-wrapper");
+wrapper.style.marginLeft="-640px";
+
+}
+
+function mostrarImagenTres() {
+  var wrapper = document.getElementsByClassName("slider-slides-wrapper");
+wrapper.style.marginLeft="-1280px";
+
+}
