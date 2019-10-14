d0af9979dca83b9d6b1552499d936898bdd82baa
Mariana <mcstedo14@gmail.com>
2019-10-14 10:42:08 -0500

    Derecha e izquierda

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
+var control = 0;
+var margen = 0;
+/*function dots() {
+    if (control = 0) {
+        // document.getElementsById("left").style.display = "none";
+        document.getElementById("dot1").classList.add("active");
+        document.getElementById("dot2").classList.remove("active");
+        document.getElementById("dot3").classList.remove("active");
+    }
+    if (control = -1) {
+        // document.getElementsById("left").style.display = "none";
+        document.getElementById("dot1").classList.remove("active");
+        document.getElementById("dot2").classList.add("active");
+        document.getElementById("dot3").classList.remove("active");
+    }
+    if (control = -2) {
+        // document.getElementsById("left").style.display = "none";
+        document.getElementById("dot1").classList.remove("active");
+        document.getElementById("dot2").classList.remove("active");
+        document.getElementById("dot3").classList.add("active");
+    }
+}*/
+function left() {
+    if (control > -2) {
+        margen -= 640;
+        document.getElementById("slide-wrapper").style.marginLeft = margen + "px";
+        control--;
+    }
 
+}
+function right() {
+    if (control < 0) {
+        margen += 640;
+        document.getElementById("slide-wrapper").style.marginLeft = margen + "px";
+        control++;
+    }
+}
 function slide1() {
     document.getElementById("slide-wrapper").style.marginLeft = "0px";
     document.getElementById("dot1").classList.add("active");
@@ -18,3 +54,5 @@ function slide3() {
     document.getElementById("dot2").classList.remove("active");
     document.getElementById("dot3").classList.add("active");
 }
+
+dots();
