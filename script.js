712a62320194f0c25a2450419b98d3c0a04e4996
Nicole K <nicolekleimanyedid@gmail.com>
2019-10-14 21:38:34 -0500

    Ya salió el funcionamiento :D

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-function mostrarImagenUno() {
-  var wrapper = document.getElementsByClassName("slider-slides-wrapper");
+var wrapper = document.getElementById("wrapper");
+
+
+function mostrarImagenUno(){
 wrapper.style.marginLeft="0px";
 
 }
 
-function mostrarImagenDos() {
-  var wrapper = document.getElementsByClassName("slider-slides-wrapper");
-wrapper.style.marginLeft="-640px";
+function mostrarImagenDos(){
+wrapper.style.marginLeft= "-640px";
 
 }
 
-function mostrarImagenTres() {
-  var wrapper = document.getElementsByClassName("slider-slides-wrapper");
+function mostrarImagenTres(){
 wrapper.style.marginLeft="-1280px";
+
+}
+
+var control = 0;
+var mover = 0;
+function desplazarIzquierda(){
+    if (control > -2){
+        control--;
+        mover -= 640;
+        wrapper.style.marginLeft = mover + "px";
+    }
+     
+    if(document.getElementById("wrapper").style.marginLeft == "-1280px"){
+      flechaD = document.getElementById(flechaDerecha);
+      flechaD.style.display = none;
+    }
+
+}
+
+function desplazarDerecha(){
+    if (control < 0){
+        control++;
+        mover += 640;
+        document.getElementById("wrapper").style.marginLeft = mover + "px";
+    }
 
 }
