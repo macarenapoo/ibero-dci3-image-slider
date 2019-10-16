335380b527a250c3cd950b3f6dcdb955af939878
Nicole K <nicolekleimanyedid@gmail.com>
2019-10-15 21:05:44 -0500

    Ya casi me saleee, hay un fallo con las flechas

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ var boton1 = document.getElementById("boton1");
 var boton2 = document.getElementById("boton2");
 var boton3 = document.getElementById("boton3");
 var i;
+var flechaDerecha = document.getElementById("flechaDerecha");
+var flechaIzquierda = document.getElementById("flechaIzquierda");
 
-function mostrarImagenUno(){
+
+/*function mostrarImagenUno(){
 wrapper.style.marginLeft="0px";
 control = 0;
  boton1.classList.add("active");
@@ -30,7 +33,7 @@ boton3.classList.add("active");
 var flechaDerecha = document.getElementById("flechaDerecha");
 flechaDerecha.classList.add("inactive");
 }
-
+*/
 
 
 
@@ -40,26 +43,74 @@ flechaDerecha.classList.add("inactive");
   });
 }*/
 
-
 var control = 0;
 var mover = 0;
+
 function desplazarIzquierda(){
     if (control > -2){
         control--;
-        mover -= 640;
-        wrapper.style.marginLeft = mover + "px";
+        /*mover -= 640;
+        wrapper.style.marginLeft = mover + "px";*/
     }
-     
-
-     
+    buenaImagen();
+   
   
 }
 
 function desplazarDerecha(){
     if (control < 0){
         control++;
-        mover += 640;
-        document.getElementById("wrapper").style.marginLeft = mover + "px";
+        /*mover += 640;
+        document.getElementById("wrapper").style.marginLeft = mover + "px";*/
     }
+    buenaImagen();
 
 }
+
+function buenaImagen(){
+if(control == 0){
+  clickBoton1();
+  flechaIzquierda.classList.add("inactive");
+  flechaDerecha.classList.remove("inactive");
+} 
+
+if(control==-1){
+  clickBoton2();
+  flechaIzquierda.classList.remove("inactive");
+  flechaDerecha.classList.remove("inactive");
+}
+
+if(control==-2){
+ clickBoton3();
+ flechaIzquierda.classList.remove("inactive");
+flechaDerecha.classList.add("inactive");
+
+}
+}
+
+function clickBoton1(){
+  wrapper.style.marginLeft="0px";
+  control = 0;
+   boton1.classList.add("active");
+   boton2.classList.remove("active");
+   boton3.classList.remove("active");
+}
+
+function clickBoton2(){
+  wrapper.style.marginLeft= "-640px";
+control = -1;
+boton1.classList.remove("active");
+boton2.classList.add("active");
+boton3.classList.remove("active");
+}
+
+function clickBoton3(){
+  wrapper.style.marginLeft="-1280px";
+  control = -2;
+boton1.classList.remove("active");
+boton2.classList.remove("active");
+boton3.classList.add("active");
+
+
+}
+
