e52306529525c87c8419b88263fad95abdebe4e2
diegomorales98 <moralesdelamoradiego@gmail.com>
2019-10-17 18:02:02 -0500

    final

--- script.js
+++ script.js
@@ -6,38 +6,74 @@ function flechaIzquierda(){
         control++;
         mover += 640;
         document.getElementById("slide").style.marginLeft = mover + "px";
+        puntoActivo();
+        flechaFanctasma();
     }
 
 }
 
 function flechaDerecha(){
-    
     if (control > -2){
         control--;
         mover -= 640;
         document.getElementById("slide").style.marginLeft = mover + "px";
-       
+       puntoActivo();
+        flechaFanctasma();
     }
     
 }
 
 function puntoUno(){
-    document.getElementById("slide").style.marginLeft="0px";
-    document.getElementById("punto1").classList.add("active");
-    document.getElementById("punto2").classList.add("inactive");
-    document.getElementById("punto3").classList.add("inactive");
+    mover = 0;
+    control = 0;
+    document.getElementById("slide").style.marginLeft=mover+"px";
+    puntoActivo();
+    flechaFanctasma();
 }
 
 function puntoDos(){
-    document.getElementById("slide").style.marginLeft="-640px";
-    document.getElementById("punto2").classList.add("active");
-    document.getElementById("punto3").classList.add("inactive");
-    document.getElementById("punto1").classList.add("inactive");
+    mover = -640;
+    control = -1;
+    document.getElementById("slide").style.marginLeft=mover+"px";
+    puntoActivo();
+    flechaFanctasma();
 }
 
 function puntoTres(){
-    document.getElementById("slide").style.marginLeft="-1280px";
-    document.getElementById("punto3").classList.add("active");
-    document.getElementById("punto1").classList.add("inactive");
-    document.getElementById("punto2").classList.add("inactive");
+    mover = -1280;
+    control = -2;
+    document.getElementById("slide").style.marginLeft=mover+"px";
+    flechaFanctasma();
 }
+
+    function puntoActivo(){
+    if (control == 0){
+        document.getElementById("punto1").classList.add("active");
+    }else{
+        document.getElementById("punto1").classList.remove("active");
+    }
+    if (control == -1){
+        document.getElementById("punto2").classList.add("active");
+    }else{
+    document.getElementById("punto2").classList.remove("active");  
+    }
+    if (control == -2){
+        document.getElementById("punto3").classList.add("active");
+    }else{
+        document.getElementById("punto3").classList.remove("active");
+    }
+    }
+    function flechaFanctasma(){
+        if(control==0){
+            document.getElementById("flechaizquierda").style.visibility="hidden";
+        }else{
+            document.getElementById("flechaizquierda").style.visibility="visible";
+        }
+        if(control==-2){
+            document.getElementById("flechaderecha").style.visibility="hidden";
+        }else{
+            document.getElementById("flechaderecha").style.visibility="visible";
+        }
+    }
+puntoActivo();
+flechaFanctasma();
