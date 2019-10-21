bc1fac48304a7e48d924c5a6849230d2f033c90a
morado20 <morado20@morado20.id.dis.ibero.mx>
2019-10-21 09:44:17 -0500

    Esconder flechas

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var wrapper = document.getElementById("wrapper");
 var slideWidth = document.getElementById("slider").offsetWidth;
 var currentSlide = 0;
+var slides = document.getElementsByClassName("slider-slide");
 
 function nextSlide() {
    currentSlide++;
@@ -14,4 +15,23 @@ function prevSlide() {
 
 function updateWrapperPosition() {
     wrapper.style.marginLeft = - (slideWidth * currentSlide) + "px";
+    updateArrows();
 }
+
+function updateArrows() {
+    var leftArrow = document.getElementById("leftArrow");
+    var rightArrow = document.getElementById("rightArrow");
+    if (currentSlide === 0) {
+        leftArrow.style.visibility = "hidden";
+    } else {
+        leftArrow.style.visibility = "visible";
+    }
+
+    if (currentSlide === slides.length - 1) {
+        rightArrow.style.visibility = "hidden";
+    } else {
+        rightArrow.style.visibility = "visible";
+    }
+}
+
+updateArrows();
